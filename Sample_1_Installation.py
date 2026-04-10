bc85a8c75712399d82cc9fc804e8c2e20fc36b00
Alvin <m4160299@gmail.com>
2026-04-10 13:25:00 +0800

    Create Sample_1_Installation.py

---
+++ Sample_1_Installation.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My First Game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
