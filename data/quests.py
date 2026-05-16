2bd2693310d477ff72ed0c38b094ebc074c86364
Alvin <m4160299@gmail.com>
2026-05-16 16:09:28 +0800

    Create quests.py

---
+++ data/quests.py
@@ -0,0 +1,5 @@
+quest_descriptions = {
+    "clue_gogo_boots": "Find the Go-Go Boots",
+    "old_tailor_hint": "Use the Tailor's advice to find what does not belong",
+    "boots_recovered": "Complete the mini-game to stabilize the timeline"
+}
