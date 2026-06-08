2d7e46326acda3baa8b1bd73157bd03cf26cfcb5
leia <azaleiaaishah25@gmail.com>
2026-06-08 10:07:01 +0800

    Added colours,font and started the game variables

--- 1960sMiniGame.py
+++ 1960sMiniGame.py
@@ -18,3 +18,13 @@ MOD_ORANGE     = (242, 100, 25)
 MUSTARD_YELLOW = (246, 190, 0)
 CHERRY_RED     = (213, 0, 50)
 CULTURE_BLACK  = (20, 20, 20)
+
+#fonts
+font_tittle =pygame.font.SysFont('Arial', 60)
+font_bold = pygame.font.SysFont("courier", 22, bold=True)
+font_text = pygame.font.SysFont("courier", 18)
+
+# The game variables
+score = 0
+grid_timer = 0
+color_flip_rate = 90
