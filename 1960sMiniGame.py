c301e61b01d05b7ce3244d3ba383903f8f23f46d
leia <azaleiaaishah25@gmail.com>
2026-06-08 09:58:45 +0800

    Added colours for this game hehehe

--- 1960sMiniGame.py
+++ 1960sMiniGame.py
@@ -5,6 +5,16 @@ import sys
 pygame.init()
 
 # Screen settings
-WIDTH, HEIGHT = 800, 600
-fps = 60
+WIDTH, HEIGHT = 1200, 800
+FPS = 60
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+# Colours for the game
+
+BLACK          = (10, 10, 12)
+WHITE          = (255, 255, 255)
+MOD_ORANGE     = (242, 100, 25)
+MUSTARD_YELLOW = (246, 190, 0)
+CHERRY_RED     = (213, 0, 50)
+CULTURE_BLACK  = (20, 20, 20)
