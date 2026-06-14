635974974cf0eea061c700ab52092f832e9acd0d
leia <azaleiaaishah25@gmail.com>
2026-06-15 06:44:35 +0800

    Added screen and colours

--- 1980sMinigame.py
+++ 1980sMinigame.py
@@ -2,4 +2,27 @@ import pygame
 import random
 import time
 
-py.game.init()
+pygame.init()
+
+# Screen
+WIDTH, HEIGHT = 1200, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Simon Says - 1980s Security Panel")
+
+# Colors
+RED = (200, 50, 50)
+GREEN = (50, 200, 50)
+BLUE = (50, 50, 200)
+YELLOW = (200, 200, 50)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+colors = [RED, GREEN, BLUE, YELLOW]
+
+# Button positions
+buttons = [
+    pygame.Rect(100, 100, 180, 180),  # red
+    pygame.Rect(320, 100, 180, 180),  # green
+    pygame.Rect(100, 320, 180, 180),  # blue
+    pygame.Rect(320, 320, 180, 180),  # yellow
+]
