0c085a0a2b9be71e1b14498fc1ea7c909e943498
leia <azaleiaaishah25@gmail.com>
2026-06-15 12:30:27 +0800

    game mechanics starting no!

--- 1990sMiniGame.py
+++ 1990sMiniGame.py
@@ -22,3 +22,34 @@ GRAY = (180, 180, 180)
 #fonts 
 font_title = pygame.font.SysFont("Arial", 40, bold=True)
 font_text = pygame.font.SysFont("Courier", 24)
+
+# the puzzle grid and settings
+
+GRID_SIZE = 3
+IMAGE_SIZE = 600
+TILE_SIZE = IMAGE_SIZE // GRID_SIZE
+
+OFFSET_X = 200
+OFFSET_Y = 120
+
+# image loader
+
+image = pygame.image.load("downloads/pearl_necklace.jpg").convert()
+image = pygame.transform.scale(image, (IMAGE_SIZE, IMAGE_SIZE))
+
+# Split image into pieces
+tiles = []
+
+for row in range(GRID_SIZE):
+    for col in range(GRID_SIZE):
+        tile = image.subsurface(
+            pygame.Rect(
+                col * TILE_SIZE,
+                row * TILE_SIZE,
+                TILE_SIZE,
+                TILE_SIZE
+            )
+        ).copy()
+
+        tiles.append(tile)
+
