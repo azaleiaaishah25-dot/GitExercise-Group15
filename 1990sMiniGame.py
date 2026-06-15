888c4af740379fcb4dea13bd510e6db206ba71c7
leia <azaleiaaishah25@gmail.com>
2026-06-15 12:24:51 +0800

    added colour and fonts

--- 1990sMiniGame.py
+++ 1990sMiniGame.py
@@ -5,3 +5,20 @@ import time
 
 pygame.init()
 
+#Screen size
+
+WIDTH, HEIGHT = 800, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("1990s Archive Puzzle")
+
+clock = pygame.time.Clock()
+
+# colours
+
+WHITE = (245, 245, 245)
+BLACK = (20, 20, 20)
+GRAY = (180, 180, 180)
+
+#fonts 
+font_title = pygame.font.SysFont("Arial", 40, bold=True)
+font_text = pygame.font.SysFont("Courier", 24)
